6ba8d181bc431d1f7eff6dd0e2190e0c1409a283
Siyath Pandigama <siyathpadigama2003@gmail.com>
2024-12-10 23:06:44 +0530

    Changes:- -Moved the contents of ApplicationCLI back into Main. -Removed unnecessary code snippets -Changed the project framework into Maven

--- src/main/java/com/siyath/util/CustomLogger.java
+++ src/main/java/com/siyath/util/CustomLogger.java
@@ -1,39 +1,39 @@
-package com.siyath.util;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class CustomLogger {
-    private static final String LOGGER = "ticketingSystem.log";
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public static synchronized void log (String type, String message){
-        String timeStamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-        String logEntry = String.format("[%s] [%s] %s",timeStamp, type, message);
-        System.out.println(logEntry);
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOGGER))){
-            writer.write(logEntry);
-            writer.newLine();
-
-        }catch (IOException e){
-            System.err.println("Error while logging " + e.getMessage());
-        }
-
-    }
-
-    public static void infoMessage (String message){
-        log("Info", message);
-    }
-
-    public static void error (String message){
-        log("Error", message);
-    }
-
-    public static void warning (String message){
-        log("Warning", message);
-    }
-}
+//package com.siyath.util;
+//
+//import java.io.BufferedWriter;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//
+//public class CustomLogger {
+//    private static final String LOGGER = "ticketingSystem.log";
+//    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//
+//    public static synchronized void log (String type, String message){
+//        String timeStamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+//        String logEntry = String.format("[%s] [%s] %s",timeStamp, type, message);
+//        System.out.println(logEntry);
+//
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOGGER))){
+//            writer.write(logEntry);
+//            writer.newLine();
+//
+//        }catch (IOException e){
+//            System.err.println("Error while logging " + e.getMessage());
+//        }
+//
+//    }
+//
+//    public static void infoMessage (String message){
+//        log("Info", message);
+//    }
+//
+//    public static void error (String message){
+//        log("Error", message);
+//    }
+//
+//    public static void warning (String message){
+//        log("Warning", message);
+//    }
+//}
